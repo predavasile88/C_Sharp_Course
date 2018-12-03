c9cca3091c39e4cee07ed8e497ef650590bc71ae
predavasile88 <predavasile88@gmail.com>
2018-12-04 01:00:46 +0200

    Commit for test

--- TestProject/TestProject/Form1.cs
+++ TestProject/TestProject/Form1.cs
@@ -16,5 +16,10 @@ namespace TestProject
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //add a code comment
+        }
     }
 }
