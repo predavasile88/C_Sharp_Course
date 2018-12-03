926996905349ebbb4131ac7471a9d53a3e8e31b6
predavasile88 <predavasile88@gmail.com>
2018-12-04 01:02:20 +0200

    Great!

--- TestProject/TestProject/Form1.cs
+++ TestProject/TestProject/Form1.cs
@@ -20,6 +20,8 @@ namespace TestProject
         private void Form1_Load(object sender, EventArgs e)
         {
             //add a code comment
+
+            //another comment
         }
     }
 }
